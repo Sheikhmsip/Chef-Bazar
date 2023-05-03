9f2474769769776ee723a55c99cd50ebd5c899b8
Sheikhmsip <sheikhmsip@gmail.com>
2023-05-03 11:56:05 +0600

    added login page

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -4,6 +4,7 @@ import Main from '../Layout/Main';
 import Home from '../pages/Home/Home/Home';
 import Login from '../pages/Login/Login/Login';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import Register from '../pages/Login/Register/Register';
 
 const router = createBrowserRouter ([
     {
@@ -18,6 +19,10 @@ const router = createBrowserRouter ([
             {
                 path: '/login',
                 element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
             }
         ]
     }
--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>
-            this is login page
+        <div className='container mx-auto bg-purple-300 rounded-lg py-2'>
+            <h3 className=' w-80 py-2 mt-2 mx-auto text-purple-100 font-bold text-lg rounded-lg bg-purple-900 text-center'>Please Logins</h3>
+            <from className="form-control w-full max-w-xs mx-auto">
+                <label className="label">
+                    <span className="label-text font-bold">What is your name ?</span>
+                    
+                </label>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+                <label className="label">
+                    <span className="label-text font-bold">What is your Email ?</span>
+                    
+                </label>
+                <input type="email" name='email' placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+                <label className="label">
+                    <span className="label-text font-bold">What is your Password?</span>
+                    
+                </label>
+                <input type="password" name='password' placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+                <button className='btn btn-accent mt-2' type='submit'>Login</button>
+
+                <p>Don't have an account <span> <Link className='link link-primary' to="/register">Registration</Link></span></p>
+                
+            </from>
         </div>
     );
 };
